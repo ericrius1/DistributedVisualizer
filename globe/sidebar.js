cae6a16e0ccb158f67829c26252e43592eaa4e40
Eric <Ericrius1@gmail.com>
2013-06-02 08:43:22 -0700

    changing style up

--- globe/sidebar.js
+++ globe/sidebar.js
@@ -1,30 +1,49 @@
 var DAT = DAT || {}
 
-DAT.SideBar = function(sidebar) {
+DAT.SideBar = function() {
 
-  var stats = d3.select("#statsSideBar");
-  var width = stats.style("width");
-  console.log(width); 
-  var sampleSVG = stats
-    .append("svg")
-    .attr("width",  stats.style("width"))
-    .attr("height",  stats.style("height"));
+  function dataInit(){
+    var $stats = $("#statsSideBar");
+  }
 
-  sampleSVG.append("circle")
-    .style("stroke", "gray")
-    .style("fill", "white")
-    .attr("r", 40)
-    .attr("cx", 50)
-    .attr("cy", 80)
-    .on("mouseover", function() {
-    d3.select(this).style("fill", "aliceblue");
-  })
-    .on("mouseout", function() {
-    d3.select(this).style("fill", "white");
-  });
+
+
+  function d3Init() {
+
+    var z = d3.scale.category20c(),
+      i = 0;
+
+    var stats = d3.select("#statsSideBar");
+    var width = stats.style("width");
+    var height = stats.style("height");
+
+    var svg = stats.append("svg:svg")
+      .attr("width", width)
+      .attr("height", height)
+      .style("pointer-events", "all")
+      .on("mousemove", particle);
+
+    function particle() {
+      var m = d3.mouse(this);
+
+      svg.append("svg:circle")
+        .attr("cx", m[0])
+        .attr("cy", m[1])
+        .attr("r", 1e-6)
+        .style("stroke", z(++i))
+        .style("stroke-opacity", 1)
+        .transition()
+        .duration(2000)
+        .ease(Math.sqrt)
+        .attr("r", 100)
+        .style("stroke-opacity", 1e-6)
+        .remove();
+    }
+
+  }
+
+  d3Init();
+
+
+
 }
-
-// usuwanie kółek
-
-// var circle = svg.selectAll("circle").data([130, 210])
-// circle.exit().remove();
