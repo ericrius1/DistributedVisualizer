457308b6bfd3cf8e6a1cfd09147dc398a4dd21b0
Eric <Ericrius1@gmail.com>
2013-06-02 14:40:52 -0700

    working with firebase

--- globe/globe.js
+++ globe/globe.js
@@ -217,7 +217,6 @@ DAT.Globe = function(container) {
       opts.animated = opts.animated || false;
     this.is_animated = opts.animated;
     opts.format = opts.format || 'magnitude'; // other option is 'legend'
-    console.log(opts.format);
     if (opts.format === 'magnitude') {
       step = 3;
     } else {
